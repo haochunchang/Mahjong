0aea248093166345e81a0d9b92fe3def8e67ca7d
haochunchang <b03b02014@ntu.edu.tw>
2018-01-13 13:31:05 +0800

    get clear_info

--- module/MJplayer.cpp
+++ module/MJplayer.cpp
@@ -130,6 +130,12 @@ MJtile MJplayer::play(int index) {
     return _hand.play(index);
 }
 
+
+void MJplayer::clear_info(void) {
+    out = vector<vector<int> >(4, vector<int>(9, 0));
+}
+
+
 // actiontype: nothing=0 eat=1 pong=2 minggone=3 angone=4 bugone=5 hu=7
 void MJplayer::act(int type, int param, MJtile& t, MJcollection& mjcol) {
     int index = _hand.total_len() - _hand.faceup_len();
